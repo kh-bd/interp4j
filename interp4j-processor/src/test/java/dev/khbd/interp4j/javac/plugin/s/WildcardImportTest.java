9e4a3ba64a0e89814d1dfc0b103e57bc2fcf43a7
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-05-22 20:49:55 +0300

    Support wildcard imports.
    (cherry picked from commit 0fc86fe52af563aca1b15da04c54d5332f252d08)

---
+++ interp4j-processor/src/test/java/dev/khbd/interp4j/javac/plugin/s/WildcardImportTest.java
@@ -0,0 +1,69 @@
+package dev.khbd.interp4j.javac.plugin.s;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Sergei_Khadanovich
+ */
+public class WildcardImportTest extends AbstractPluginTest {
+
+    @Test(dataProvider = "optionsDataProvider")
+    public void interpolate_wildcardMethodsImport_interpolate(PluginOptions options) throws Exception {
+        String source = """
+                package cases.wildcard_import;
+                
+                import static dev.khbd.interp4j.core.Interpolations.*;
+                
+                public class Main {
+                
+                    public static String greet() {
+                        String name = "Alex";
+                        return s("I'm ${name}. ") + s("I'm ${name.toUpperCase()}");
+                    }
+                }
+                """;
+
+        CompilationResult result = compiler.compile(options, "cases/wildcard_import/Main.java", source);
+
+        assertThat(result.isSuccess()).isTrue();
+
+        ClassLoader classLoader = result.getClassLoader();
+        Class<?> clazz = classLoader.loadClass("cases.wildcard_import.Main");
+        Method method = clazz.getMethod("greet");
+        String greet = (String) method.invoke(null);
+
+        assertThat(greet).isEqualTo("I'm Alex. I'm ALEX");
+    }
+
+    @Test(dataProvider = "optionsDataProvider")
+    public void interpolate_wildcardPackageImport_interpolate(PluginOptions options) throws Exception {
+        String source = """
+                package cases.wildcard_import;
+                
+                import dev.khbd.interp4j.core.*;
+                
+                public class Main {
+                
+                    public static String greet() {
+                        String name = "Alex";
+                        return Interpolations.s("I'm ${name}. ") + Interpolations.s("I'm ${name.toUpperCase()}");
+                    }
+                }
+                """;
+
+        CompilationResult result = compiler.compile(options, "cases/wildcard_import/Main.java", source);
+
+        assertThat(result.isSuccess()).isTrue();
+
+        ClassLoader classLoader = result.getClassLoader();
+        Class<?> clazz = classLoader.loadClass("cases.wildcard_import.Main");
+        Method method = clazz.getMethod("greet");
+        String greet = (String) method.invoke(null);
+
+        assertThat(greet).isEqualTo("I'm Alex. I'm ALEX");
+    }
+}
