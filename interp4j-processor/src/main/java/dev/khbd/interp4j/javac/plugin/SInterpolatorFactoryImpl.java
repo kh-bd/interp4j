2ac0afc799269441d296baeb2887369265fd0ed1
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-05-24 16:16:10 +0300

    Extract `AbstractInterpolationFactory`.

--- interp4j-processor/src/main/java/dev/khbd/interp4j/javac/plugin/SInterpolatorFactoryImpl.java
+++ interp4j-processor/src/main/java/dev/khbd/interp4j/javac/plugin/SInterpolatorFactoryImpl.java
@@ -3,7 +3,6 @@ package dev.khbd.interp4j.javac.plugin;
 import com.sun.source.tree.CompilationUnitTree;
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.LiteralTree;
-import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.Tree;
 import com.sun.tools.javac.parser.JavacParser;
 import com.sun.tools.javac.parser.ParserFactory;
@@ -19,62 +18,34 @@ import dev.khbd.interp4j.javac.plugin.s.SExpressionVisitor;
 import dev.khbd.interp4j.javac.plugin.s.SText;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.function.Predicate;
 
 /**
  * @author Sergei Khadanovich
  */
-class SInterpolatorFactoryImpl implements InterpolatorFactory {
+class SInterpolatorFactoryImpl extends AbstractInterpolatorFactory {
 
-    private static final Predicate<Tree> METHOD_PREDICATE = PluginUtils.pathPredicate(Interpolation.S.getMethod());
-    private static final Predicate<Tree> QUALIFIED_METHOD_PREDICATE =
-            PluginUtils.pathPredicate(Interpolation.S.getClassName(), Interpolation.S.getMethod());
-    private static final Predicate<Tree> FQN_PREDICATE =
-            PluginUtils.pathPredicate(Interpolation.S.getPackageName(), Interpolation.S.getClassName(), Interpolation.S.getMethod());
+    SInterpolatorFactoryImpl() {
+        super(Interpolation.S);
+    }
 
     @Override
     public Interpolator create(Context context, CompilationUnitTree unit) {
-        Imports imports = Imports.collector(Interpolation.S).collect(unit.getImports());
-        return new SInterpolator(imports, Log.instance(context), TreeMaker.instance(context), ParserFactory.instance(context));
+        return new SInterpolator(context, unit);
     }
 
-    @RequiredArgsConstructor
-    private static class SInterpolator implements Interpolator {
+    private class SInterpolator extends AbstractInterpolator {
 
-        private final Imports imports;
         private final Log logger;
         private final TreeMaker treeMaker;
         private final ParserFactory parserFactory;
 
-        @Override
-        public boolean isInterpolateCall(JCTree.JCMethodInvocation invocation) {
-            if (!isSMethodInvocation(invocation)) {
-                return false;
-            }
+        SInterpolator(Context context, CompilationUnitTree unit) {
+            super(unit);
 
-            List<? extends ExpressionTree> arguments = invocation.getArguments();
-
-            // otherwise, it's a compile-time error
-            return arguments.size() == 1;
-        }
-
-        private boolean isSMethodInvocation(MethodInvocationTree methodInvocation) {
-            ExpressionTree methodSelect = methodInvocation.getMethodSelect();
-
-            // s() method call
-            if (METHOD_PREDICATE.test(methodSelect)) {
-                return imports.isSimpleMethodCallAllowed();
-            }
-
-            // Interpolations.s() method call
-            if (QUALIFIED_METHOD_PREDICATE.test(methodSelect)) {
-                return imports.isQualifiedMethodCallAllowed();
-            }
-
-            // FQN.s() method call
-            return FQN_PREDICATE.test(methodSelect);
+            this.logger = Log.instance(context);
+            this.treeMaker = TreeMaker.instance(context);
+            this.parserFactory = ParserFactory.instance(context);
         }
 
         @Override
