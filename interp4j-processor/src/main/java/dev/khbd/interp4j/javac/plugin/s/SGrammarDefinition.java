4ed7516d1176356b3b5c944a3ceff11afee7ae5a
Sergei Khadanovich <khadanovich.sergey@gmail.com>
2024-05-24 12:41:06 +0300

    Add format parser.
    (cherry picked from commit 20175b412d9da6706602acaf1df90dc51d3b5897)

--- interp4j-processor/src/main/java/dev/khbd/interp4j/javac/plugin/s/SGrammarDefinition.java
+++ interp4j-processor/src/main/java/dev/khbd/interp4j/javac/plugin/s/SGrammarDefinition.java
@@ -10,41 +10,41 @@ import org.petitparser.tools.GrammarDefinition;
 import java.util.List;
 
 /**
- * 'S' expression grammar.
+ * 'S' code grammar.
  *
  * @author Sergei_Khadanovich
  */
 class SGrammarDefinition extends GrammarDefinition {
 
     private static final String TEXT = "text";
-    private static final String EXPRESSION = "expression";
-    private static final String EXPRESSION_AND_TEXT = "expressionAndText";
+    private static final String CODE = "code";
+    private static final String CODE_AND_TEXT = "codeAndText";
 
     SGrammarDefinition() {
         def("start",
                 ref(TEXT)
-                        .seq(ref(EXPRESSION_AND_TEXT).star())
+                        .seq(ref(CODE_AND_TEXT).star())
                         .end()
         );
         action("start", (List<Object> seq) -> {
             SExpression expression = new SExpression();
             addNotEmptyTextPart(expression, (SText) seq.get(0));
             if (seq.size() > 1) {
-                List<ExpressionAndText> other = (List<ExpressionAndText>) seq.get(1);
-                for (ExpressionAndText exprAndText : other) {
-                    expression.addPart(exprAndText.getExpression());
-                    addNotEmptyTextPart(expression, exprAndText.getText());
+                List<CodeAndText> other = (List<CodeAndText>) seq.get(1);
+                for (CodeAndText andText : other) {
+                    expression.addPart(andText.getCode());
+                    addNotEmptyTextPart(expression, andText.getText());
                 }
             }
             return expression;
         });
 
-        def(EXPRESSION_AND_TEXT, ref(EXPRESSION).seq(ref(TEXT)));
-        action(EXPRESSION_AND_TEXT, (List<Object> seq) ->
-                new ExpressionAndText((SCode) seq.get(0), (SText) seq.get(1)));
+        def(CODE_AND_TEXT, ref(CODE).seq(ref(TEXT)));
+        action(CODE_AND_TEXT, (List<Object> seq) ->
+                new CodeAndText((SCode) seq.get(0), (SText) seq.get(1)));
 
-        def(EXPRESSION, expressionWithBrackets().or(expressionWithoutBrackets()));
-        action(EXPRESSION, (Token token) -> new SCode(token.getValue(), new Position(token.getStart(), token.getStop())));
+        def(CODE, codeWithBrackets().or(codeWithoutBrackets()));
+        action(CODE, (Token token) -> new SCode(token.getValue(), new Position(token.getStart(), token.getStop())));
 
         def(TEXT, textParser());
         action(TEXT, (Token token) -> new SText(token.getValue(), new Position(token.getStart(), token.getStop())));
@@ -57,7 +57,7 @@ class SGrammarDefinition extends GrammarDefinition {
         expression.addPart(text);
     }
 
-    private static Parser expressionWithBrackets() {
+    private static Parser codeWithBrackets() {
         Parser openParser = StringParser.of("${");
         Parser expressionBodyParser = CharacterParser.noneOf("}").star().flatten().token();
         Parser closeParser = CharacterParser.of('}');
@@ -65,7 +65,7 @@ class SGrammarDefinition extends GrammarDefinition {
                 .map((List<Object> seq) -> seq.get(1));
     }
 
-    private static Parser expressionWithoutBrackets() {
+    private static Parser codeWithoutBrackets() {
         Parser openParser = StringParser.of("$");
         return openParser.seq(literalParser())
                 .map((List<Object> seq) -> seq.get(1));
@@ -97,8 +97,8 @@ class SGrammarDefinition extends GrammarDefinition {
     }
 
     @Value
-    private static class ExpressionAndText {
-        SCode expression;
+    private static class CodeAndText {
+        SCode code;
         SText text;
     }
 }
